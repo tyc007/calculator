40e1ea0f4a5e509d93cec0615d4df61ff8271d9e
tyc007 <tonychan550@gmail.com>
2023-03-20 15:27:43 -0700

    basic number display

--- script.js
+++ script.js
@@ -1,39 +1,82 @@
 const buttons = document.querySelectorAll('.btn');
-const input = document.getElementById('calculator-input');
+const displayHistory = document.getElementById('display-input');
+const displayInput = document.getElementById('display-current');
+const addBtn = document.querySelector('#add');
+const clearBtn = document.querySelector('#clear');
+const equalBtn = document.querySelector('#equals');
 
-console.table(buttons);
-console.log(input);
+
+// Create variables to store the first operand, operator, and second operand
+let firstOperand = '';
+let secondOperand = '';
+let operator = '';
+
 
 // Loop through all the calculator buttons and add an onclick event listener to each one
 buttons.forEach(button => {
   button.addEventListener('click', () => {
     const buttonValue = button.textContent;
-    input.value += buttonValue;
-  });
+    
+    if (!isNaN(buttonValue) || buttonValue === '.') {
+        displayInput.textContent += buttonValue;
+    }
+    });
+});
+
+clearBtn.addEventListener('click', () => {
+    displayInput.textContent = '';
+    firstOperand = '';
+    secondOperand = '';
+    operator = '';
+});
+
+equalBtn.addEventListener('click', () => {
+    firstOperand = input.value;
 });
 
 
-const add = function(a,b) {
+function operate(firstOperand, secondOperand, operator){
+    a = parseFloat(firstOperand);
+    b = parseFloat(secondOperand);
+    console.log(b)
+    switch (operator) {
+        case '+':
+            displayInput.value = add(a,b);
+            break;
+        case '-':
+            displayInput.value = subtract(a,b);
+            break;
+        case 'x':
+            displayInput.value = multiply(a,b);
+            break;
+        case '÷':
+            displayInput.value = divide(a,b);
+            break;
+      }
+}
+
+
+function add(a,b) {
 	return a + b;
 };
 
-const subtract = function(a,b) {
+function subtract(a,b) {
 	return a - b;
 };
 
-const sum = function(a) {
-  return a.reduce((total,current) => total + current, 0);
+function multiply(a,b) {
+	return a * b;
 };
 
-const multiply = function(a) {
-  return a.reduce((accumlator, current) => accumlator * current, 1);
+function divide(a,b) {
+	return a * b;
 };
 
-const power = function(a,b) {
+function power(a,b) {
   return a**b;
 };
 
-const factorial = function(a) {
+function factorial(a) {
   let res = 1;
   for(let i=1; i <= a; i++){
     res *= i;
